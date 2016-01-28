79b542fa420aca48a5956b260e86e92da9effb09
Thomas J Bradley <hey@thomasjbradley.ca>
2016-01-28 13:33:52 -0500

    Add git commands for version bumping

--- version-bump.sh
+++ version-bump.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-sed -i '' "s/\"version\": \"[0-9]*.[0-9]*.[0-9]*\"/\"version\": \"$1\"/g" package.json
+sed -i '' "s/\"version\": \"[0-9]*.[0-9]*.[0-9]*\"/\"version\": \"$1\"/g" package.json builder.json
 sed -i '' "s/--app-version=[0-9]*.[0-9]*.[0-9]*/--app-version=$1/g" package.json
+
+git add package.json
+git add builder.json
+git commit -m "Bump version to $1"
+git tag "v$1"
