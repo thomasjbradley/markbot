a894a4d7d93e502d1f052643e4ff969f3ff49add
Thomas J Bradley <hey@thomasjbradley.ca>
2016-02-25 19:49:41 -0500

    Version bump

--- tools/version-bump.sh
+++ tools/version-bump.sh
@@ -2,6 +2,7 @@
 
 sed -i '' "s/\"version\": \"[0-9]*.[0-9]*.[0-9]*\"/\"version\": \"$1\"/g" package.json builder.json
 sed -i '' "s/--app-version=[0-9]*.[0-9]*.[0-9]*/--app-version=$1/g" package.json
+sed -i '' "s/--build-version=[0-9]*.[0-9]*.[0-9]*/--build-version=$1/g" package.json
 
 git add package.json
 git add builder.json
