0a374841bf1fb6235a385cac3bc6f7a8e6433369
Thomas J Bradley <hey@thomasjbradley.ca>
2016-03-17 20:47:21 -0400

    Change version updating script to hook into npm version

---
+++ tools/version.js
@@ -0,0 +1,21 @@
+const
+  path = require('path'),
+  fs = require('fs'),
+  pkg = require('../package.json'),
+  bldr = require('../builder.json'),
+  updateVersionBySearch = function (key, search) {
+    pkg.scripts[key] = pkg.scripts[key].replace(new RegExp(`--${search}=[0-9]*.[0-9]*.[0-9]*`, 'g'), `--${search}=${pkg.version}`);
+  },
+  updateVersion = function (key) {
+    updateVersionBySearch(key, 'app-version');
+    updateVersionBySearch(key, 'build-version');
+  }
+  ;
+
+bldr.win.version = pkg.version;
+
+updateVersion('pack-osx');
+updateVersion('pack-win');
+
+fs.writeFileSync(path.resolve(__dirname, '../package.json'), JSON.stringify(pkg, null, 2), 'utf8');
+fs.writeFileSync(path.resolve(__dirname, '../builder.json'), JSON.stringify(bldr, null, 2), 'utf8');
