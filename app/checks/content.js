0074aafce6f68d533ccba175782be4d830a3549e
Thomas J Bradley <hey@thomasjbradley.ca>
2017-08-11 15:39:36 -0400

    Add the ability to have warnings into search/Not

--- app/checks/content.js
+++ app/checks/content.js
@@ -1,67 +1,95 @@
 'use strict';
 
 const util = require('util');
+const merge = require('merge-objects');
 const markbotMain = require('../markbot-main');
+const messageGroup = require(`${__dirname}/message-group`);
 
 const cleanRegex = function (regex) {
   return regex.replace(/\\(?!\\)/g, '');
+};
+
+const convertToCheckObject = function (search, defaultMessage) {
+  let obj = {
+    check: false,
+    regex: false,
+    message: '',
+    customMessage: '',
+    type: 'error',
+  };
+
+  if (typeof search === 'string') {
+    obj.regex = search;
+  } else {
+    if (Array.isArray(search)) {
+      if (search.length > 1) obj.message = search[1];
+      obj.regex = search[0];
+    } else {
+      obj = Object.assign(obj, search);
+
+      if (obj.check) obj.regex = obj.check;
+
+      if (Array.isArray(obj.regex)) {
+        if (obj.regex.length > 1) obj.message = obj.regex[1];
+        obj.regex = obj.regex[0];
+      }
+    }
+  }
+
+  if (!obj.message) obj.message = defaultMessage.replace(/\{\{regex\}\}/g, cleanRegex(obj.regex));
+
+  return obj;
+};
+
+const convertToHasObject = function (search) {
+  return convertToCheckObject(search, 'Expected to see this content: `{{regex}}`');
+};
+
+const convertToHasNotObject = function (search) {
+  return convertToCheckObject(search, 'Unexpected `{{regex}}` — `{{regex}}` should not be used');
 };
 
 const bypass = function (checkGroup, checkId, checkLabel) {
   markbotMain.send('check-group:item-bypass', checkGroup, checkId, checkLabel, ['Skipped because of previous errors']);
 };
 
-const findSearchErrors = function (fileContents, search) {
-  var errors = [];
+const findSearchErrors = function (fileContents, searches) {
+  let allMessages = messageGroup.new();
 
-  search.forEach(function (regex) {
-    let re = regex, error;
+  searches.forEach(function (searchItem) {
+    let search = convertToHasObject(searchItem);
 
-    if (typeof regex == 'object') {
-      re = regex[0];
-      error = regex[1];
-    } else {
-      error = `Expected to see this content: \`${cleanRegex(regex)}\``;
-    }
-
-    if (!fileContents.match(new RegExp(re, 'gm'))) {
-      errors.push(error);
+    if (!fileContents.match(new RegExp(search.regex, 'gm'))) {
+      allMessages = messageGroup.bind(search, allMessages);
     }
   });
 
-  return errors;
+  return allMessages;
 };
 
 const findSearchNotErrors = function (fileContents, searchNot) {
-  var errors = [];
+  let allMessages = messageGroup.new();
 
-  searchNot.forEach(function (regex) {
-    let re = regex, error;
+  searchNot.forEach(function (searchItem) {
+    let search = convertToHasNotObject(searchItem);
 
-    if (typeof regex == 'object') {
-      re = regex[0];
-      error = regex[1];
-    } else {
-      error = `Unexpected \`${cleanRegex(regex)}\` — \`${cleanRegex(regex)}\` should not be used`;
-    }
-
-    if (fileContents.match(new RegExp(re, 'gm'))) {
-      errors.push(error);
+    if (fileContents.match(new RegExp(search.regex, 'gm'))) {
+      allMessages = messageGroup.bind(search, allMessages);
     }
   });
 
-  return errors;
+  return allMessages;
 };
 
 const check = function (checkGroup, checkId, checkLabel, fileContents, search, searchNot, next) {
-  var errors = [];
+  let allMessages = {};
 
   markbotMain.send('check-group:item-computing', checkGroup, checkId);
 
-  if (search) errors = errors.concat(findSearchErrors(fileContents, search));
-  if (searchNot) errors = errors.concat(findSearchNotErrors(fileContents, searchNot));
+  if (search) allMessages = merge(allMessages, findSearchErrors(fileContents, search));
+  if (searchNot) allMessages = merge(allMessages, findSearchNotErrors(fileContents, searchNot));
 
-  markbotMain.send('check-group:item-complete', checkGroup, checkId, checkLabel, errors);
+  markbotMain.send('check-group:item-complete', checkGroup, checkId, checkLabel, allMessages.errors, allMessages.messages, allMessages.warnings);
   next();
 };
 
