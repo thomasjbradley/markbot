32dd743b476082d101f9870d0f82987eb790953f
Thomas J Bradley <hey@thomasjbradley.ca>
2018-02-16 14:35:44 -0500

    Add path normalization to fix a Windows bug

--- app/strip-path.js
+++ app/strip-path.js
@@ -1,3 +1,10 @@
+'use strict';
+
+const path = require('path');
+
 module.exports = function (file, fullPath) {
-  return file.replace(fullPath, '').replace(/^[\/\\]/, '');
+  const normalizedFile = path.normalize(file);
+  const normalizedFullPath = path.normalize(fullPath);
+
+  return normalizedFile.replace(normalizedFullPath, '').replace(/^[\/\\]/, '');
 };
